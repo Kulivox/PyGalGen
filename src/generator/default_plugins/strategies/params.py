cf149b4750f3d8c7527078e0d7f6f72f6e7443bc
Michal Klinka <mich.klinka@gmail.com>
2022-03-13 20:06:00 +0100

    feat(default_plugin): started working on default plugin params section

--- src/generator/default_plugins/strategies/params.py
+++ src/generator/default_plugins/strategies/params.py
@@ -1,8 +1,9 @@
-from typing import Any
-
+from typing import Any, Iterable
+import xml.etree.ElementTree as ET
 from generator.pluggability.strategy import Strategy
 from generator.common.stages import StrategyStage
-
+from generator.common.params.argument_parser_conversion import\
+    obtain_and_convert_parser
 
 class DefaultParams(Strategy):
     STAGE = StrategyStage.PARAMS
@@ -10,5 +11,13 @@ class DefaultParams(Strategy):
     def __init__(self, args: Any):
         super(DefaultParams, self).__init__(args, self.STAGE)
 
-    def apply_strategy(self, tool_input: Any, xml_output: Any) -> Any:
-        pass
+    def apply_strategy(self, tool_input: Any, xml_output: ET.ElementTree)\
+            -> ET.ElementTree:
+        # root = xml_output.getroot()
+        # inputs = root.find("/tool/inputs")
+
+        obtain_and_convert_parser(self.args.path)
+
+        return xml_output
+
+
