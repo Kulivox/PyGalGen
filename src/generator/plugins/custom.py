a447d12f25e1a7aa0ec34f844e8a00056388bf8c
Michal Klinka <mich.klinka@gmail.com>
2022-03-15 13:35:26 +0100

    chore(plugins): removed useless plugins

--- src/generator/plugins/custom.py
+++
@@ -1,6 +0,0 @@
-from generator.pluggability.plugin import Plugin
-from generator.plugins.custom_strategy import CustomStrategy
-
-class CustomPlugin(Plugin):
-    def get_strategies(self, args):
-        return [CustomStrategy(args)]
