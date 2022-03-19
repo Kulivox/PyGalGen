98c66ee2ab7dfcabff6f4ddb4aa9adc2d0129ece
Michal Klinka <mich.klinka@gmail.com>
2022-03-19 12:57:20 +0100

    feat(linter): created basic linter

---
+++ src/linter/linter.py
@@ -0,0 +1,39 @@
+import argparse
+import lxml.etree as ET
+
+from common.utils import LINTER_MAGIC
+
+
+def magic_found(element: ET.Element):
+    if LINTER_MAGIC in element.tag:
+        return True
+
+    for name, value in element.attrib.keys():
+        if LINTER_MAGIC in name or LINTER_MAGIC in value:
+            return True
+
+    return LINTER_MAGIC in element.text
+
+
+def report_problems(element: ET.Element):
+    if magic_found(element):
+        print(f"Problem found at line {element.sourceline}")
+
+    for child in element:
+        report_problems(child)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, help="Path to input file",
+                        required=True)
+    args = parser.parse_args()
+
+    tree = ET.parse(args.path)
+    report_problems(tree.getroot())
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
